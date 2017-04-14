2ba4695a8a9bf0e565a24bba3d243ab3776b9a0e
Ben Currie <benjamin.w.currie@gmail.com>
2017-04-14 16:08:04 +0100

    had duplicate migrations so deleted them and all working again now

--- db/migrate/20170414114856_create_join_table_jobs_categories.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableJobsCategories < ActiveRecord::Migration[5.0]
-  def change
-    create_join_table :jobs, :categories do |t|
-      # t.index [:job_id, :category_id]
-      # t.index [:category_id, :job_id]
-    end
-  end
-end
