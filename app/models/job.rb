0af3556d8f142adf37bb35e67ace3847dc6112c0
Ben Currie <benjamin.w.currie@gmail.com>
2017-04-14 16:35:25 +0100

    added a chosen applicant method

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,6 @@ class Job < ApplicationRecord
   belongs_to :user
   has_many :pictures
 
+  has_one :chosen_applicant, class_name: 'User', foreign_key: 'chosen_applicant_id'
+
 end
--- app/serializers/job_serializer.rb
+++ app/serializers/job_serializer.rb
@@ -1,6 +1,6 @@
 class JobSerializer < ActiveModel::Serializer
   has_one :user
-  has_one :accepted_applicant
+  has_one :chosen_applicant_id
 
   attributes :id, :title, :description, :date, :hourly_rate, :address_line1, :address_line2, :address_line3, :address_line4
 end
