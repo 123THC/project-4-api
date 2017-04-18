8e60ab88e580a9dbb8334f647dbc50f66ba56f40
Ben Currie <benjamin.w.currie@gmail.com>
2017-04-18 15:31:10 +0100

    sorted seeds file with list of available categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+[Category, User, Job].each do |model|
+  ActiveRecord::Base.connection.execute("TRUNCATE #{model.table_name} RESTART IDENTITY CASCADE")
+end
 
-Category.create!(name: 'Heavy Lifting', image: '/Users/123THC/development/project-4-api/Category Logos/Screen Shot 2017-04-18 at 12.13.47.png');
-
-Category.create!(name: 'Home', image: '/Users/123THC/development/project-4-api/Category Logos/Screen Shot 2017-04-18 at 12.16.22.png');
+Category.create!(name: 'Heavy Lifting', image: 'images/heavylifting.png');
+Category.create!(name: 'Home Maintenance', image: 'images/home.png');
+Category.create!(name: 'Sales', image: 'images/sales.png');
+Category.create!(name: 'Maintenance', image: 'images/maintenance.png');
+Category.create!(name: 'Cleaning', image: 'images/cleaning.png');
+Category.create!(name: 'Gardening', image: 'images/gardening.png');
+Category.create!(name: 'High Pay', image: 'images/wellpaid.png');
 
 User.create!(username: 'Ben Currie', email: 'Ben@currie.co.uk', password: 'password', address_line1: '1 South Norwood Hill', address_line2: 'South Norwood', address_line3: 'London', address_line4: 'SE25 6BY', image: 'nothing yet')
 User.create!(username: 'Beth Talbot', email: 'Beth@talbot.co.uk', password: 'password', address_line1: '1 South Norwood Hill', address_line2: 'South Norwood', address_line3: 'London', address_line4: 'SE25 6BY', image: 'nothing yet')
@@ -17,4 +17,4 @@ Job.create!(title: 'Changing tyre on my car', description: 'I need the tyres on 
 
 Job.create!(title: 'Putting up shelf in Kitchen', description: 'I need someone to put up a shelf in my kitchen', hourly_rate: 12, date: '2017-04-21', address_line1: '1 South Norwood Hill', address_line2: 'South Norwood', address_line3: 'London', address_line4: 'SE25 6BY', user_id: 2, chosen_applicant_id: 1)
 
-Job.create!(title: 'Dismantling my bed', description: 'I need someone to dismantle my bed and put up the new one', hourly_rate: 8, date: '2017-05-01', address_line1: '1 South Norwood Hill', address_line2: 'South Norwood', address_line3: 'London', address_line4: 'SE25 6BY', user_id: 2, chosen_applicant_id: 1,)
+Job.create!(title: 'Dismantling my bed', description: 'I need someone to dismantle my bed and put up the new one', hourly_rate: 8, date: '2017-05-01', address_line1: '1 South Norwood Hill', address_line2: 'South Norwood', address_line3: 'London', address_line4: 'SE25 6BY', user_id: 2, chosen_applicant_id: 1)
