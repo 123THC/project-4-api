2029f4a93649b1f046fe74dbbf7653a00afb199d
Ben Currie <benjamin.w.currie@gmail.com>
2017-04-24 19:49:43 +0100

    edited seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,6 @@ User.create!(username: 'Beth Talbot', email: 'Beth@talbot.co.uk', password: 'pas
 
 Job.create!(title: 'Changing tyre on my car', description: 'I need the tyres on my car changed over but Im too old and frail', hourly_rate: 10, date: '2017-06-15', address_line1: '1 South Norwood Hill', address_line2: 'South Norwood', address_line3: 'London', address_line4: 'SE25 6BY', user_id: 1)
 
-Job.create!(title: 'Putting up shelf in Kitchen', description: 'I need someone to put up a shelf in my kitchen', hourly_rate: 12, date: '2017-04-21', address_line1: '1 South Norwood Hill', address_line2: 'South Norwood', address_line3: 'London', address_line4: 'SE25 6BY', user_id: 2, chosen_applicant_id: 1)
+Job.create!(title: 'Putting up shelf in Kitchen', description: 'I need someone to put up a shelf in my kitchen', hourly_rate: 12, date: '2017-04-21', address_line1: '1 South Norwood Hill', address_line2: 'South Norwood', address_line3: 'London', address_line4: 'SE25 6BY', user_id: 2)
 
-Job.create!(title: 'Dismantling my bed', description: 'I need someone to dismantle my bed and put up the new one', hourly_rate: 8, date: '2017-05-01', address_line1: '1 South Norwood Hill', address_line2: 'South Norwood', address_line3: 'London', address_line4: 'SE25 6BY', user_id: 2, chosen_applicant_id: 1)
+Job.create!(title: 'Dismantling my bed', description: 'I need someone to dismantle my bed and put up the new one', hourly_rate: 8, date: '2017-05-01', address_line1: '1 South Norwood Hill', address_line2: 'South Norwood', address_line3: 'London', address_line4: 'SE25 6BY', user_id: 2)
