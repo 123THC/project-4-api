220a1be21921a3df931858a8adf3233e5a78c14a
Ben Currie <benjamin.w.currie@gmail.com>
2017-05-03 15:43:43 +0100

    trying to fix cors issue

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['localhost:7000', 'https://warm-reef-46815.herokuapp.com']
+    origins ['localhost:7000', 'https://warm-reef-46815.herokuapp.com', 'https://infinite-retreat-22521.herokuapp.com']
     resource '*', headers: :any, methods: :any
   end
 end
